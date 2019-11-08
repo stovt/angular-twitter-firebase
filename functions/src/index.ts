36d98b778610e1521ee4b28c83e4e60c01f350de
Sergey Tovt <st@norse.co>
2019-11-08 10:25:34 +0200

    update cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -14,23 +14,46 @@ interface User {
 exports.updateTweetUser = functions.firestore.document('users/{userId}').onUpdate(change => {
   const updatedUser = change.after.data() as User;
 
-  const tweetRef = admin.firestore().collection('tweets');
+  const tweetsRef = admin.firestore().collection('tweets');
 
-  tweetRef
+  return tweetsRef
     .where('user.userId', '==', updatedUser.userId)
     .get()
-    .then(querySnapshot => {
-      console.log('TCL: querySnapshot', querySnapshot.docs);
-      querySnapshot.docs.forEach(doc => {
-        console.log('TCL: doc', doc);
-        tweetRef
+    .then(querySnapshot => 
+      querySnapshot.docs.forEach(doc => 
+        tweetsRef
           .doc(doc.id)
           .update({ user: updatedUser })
           .then(() => console.log(`Tweet with id: ${doc.id} has been successfully updated`))
-          .catch(() => console.log(`Something went wrong while updating tweet with id: ${doc.id}`));
-      });
-    })
+          .catch(() => console.log(`Something went wrong while updating tweet with id: ${doc.id}`))
+      )
+    )
     .catch(() => console.log('Something went wrong while fetching user tweets :('));
-  
-  return change.after.ref.set(updatedUser);
+});
+
+exports.deleteUser = functions.auth.user().onDelete(user => {
+  return admin
+    .firestore()
+    .doc(`users/${user.uid}`)
+    .delete()
+    .then(() => console.log(`User with id: ${user.uid} has been successfully deleted`))
+    .catch(() => console.log(`Something went wrong while deleting user with id: ${user.uid}`));
+});
+
+exports.deleteUserTweets = functions.auth.user().onDelete(user => {
+  const tweetsRef = admin.firestore().collection('tweets');
+
+  return tweetsRef
+    .where('user.userId', '==', user.uid)
+    .get()
+    .then(querySnapshot => 
+      querySnapshot.docs.forEach(doc => 
+        tweetsRef
+          .doc(doc.id)
+          .delete()
+          .then(() => console.log(`Tweet with id: ${doc.id} has been successfully deleted`))
+          .catch(() => console.log(`Something went wrong while deleting tweet with id: ${doc.id}`))
+      )
+    )
+    .catch(() => console.log('Something went wrong while fetching user tweets :('));
 });
